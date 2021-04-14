1713c52f83a0e2b6e1e25bf990e791d2fb0891d4
Arpit Jhunjhunwala <34285920+arpitjjw@users.noreply.github.com>
2021-04-15 05:26:41 +0530

    Update Alebo.py
    Added function to generate random points in the embedding using rejection sampling

--- pygpbo/hdbo/Alebo.py
+++ pygpbo/hdbo/Alebo.py
@@ -1,2 +1,11 @@
 # TODO: Create a class for ALEBO method for High Dimensional Bayesian Optimization
 # Reference: https://arxiv.org/abs/2001.11659
+def gen(n,d,B):
+  results=[]
+  while len(results)<n:
+    x=np.random.rand(d,1)
+    B_inv=np.linalg.pinv(B)
+
+    if (all(B_inv@x)<=1 and all(B_inv@x)>=-1):
+      results.append(x)  
+  return (np.array(results).T).reshape(n,d)
