4e1e5de788ecba2bf24632ae1669d50a499e0d50
Arpit Jhunjhunwala <34285920+arpitjjw@users.noreply.github.com>
2021-04-18 21:53:41 +0530

    Added function to generate projection matrix

--- pygpbo/hdbo/Hesbo.py
+++ pygpbo/hdbo/Hesbo.py
@@ -1,2 +1,11 @@
 # TODO: Create a class for HesBO method for High Dimensional Bayesian Optimization
 # Reference: http://proceedings.mlr.press/v97/nayebi19a.html
+import random
+import numpy as np
+def gen_A(m,n):
+  # m x n dimension
+  A=np.zeros(shape=(m,n))
+  for i in range(m):
+    rand_index=np.random.randint(low=0,high=n,size=1)
+    A[i,rand_index]=random.choice([1,-1])
+  return A  
