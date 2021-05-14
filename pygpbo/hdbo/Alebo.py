79aac028a323073b820b47ff3744b4bf31436296
harshit987 <hakumar@iitk.ac.in>
2021-05-15 00:10:04 +0530

    Correction in gen gunction

--- pygpbo/hdbo/Alebo.py
+++ pygpbo/hdbo/Alebo.py
@@ -1,11 +1,14 @@
 # TODO: Create a class for ALEBO method for High Dimensional Bayesian Optimization
 # Reference: https://arxiv.org/abs/2001.11659
 def gen(n,d,B):
-  results=[]
+  results=np.array([])
   while len(results)<n:
     x=np.random.rand(d,1)
+    D = B.shape[1]
     B_inv=np.linalg.pinv(B)
-
-    if (all(B_inv@x)<=1 and all(B_inv@x)>=-1):
-      results.append(x)  
-  return (np.array(results).T).reshape(n,d)
+    if ((B_inv@x)<=1).sum()== D and ((B_inv@x)>=-1).sum() == D:
+      if results.size==-0:
+        results = x.T
+      else:
+        results = np.concatenate((results,x.T),axis=0)
+  return results
